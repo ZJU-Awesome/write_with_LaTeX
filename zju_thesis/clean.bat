7c69eeefdb2f93a2c5e221f85692be56b8f53ba4
sky chan <diufanshu@gmail.com>
2014-06-30 20:27:28 +0800

    add the prepare file
    as the beginning

---
+++ zju_thesis/clean.bat
@@ -0,0 +1,14 @@
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.out /s
+del *.lof /s
+del *.lot /s
+del *.synctex.gz /s
+del *.synctex.gz(busy) /s
+del *.fdb_latexmk /s
---
+++ zju_thesis/make.bat
@@ -0,0 +1,37 @@
+@echo off
+title Monster XeLaTeX Template
+:start
+cls
+echo 欢迎使用老怪 XeLaTeX 学位论文模板，请指示您的需要~
+echo ----------------
+echo 0. 清理冗余文件
+echo 1. 测试 prepare
+echo 2. 退出
+echo ----------------
+set /p num="请输入您的选择: "
+if "%num%"=="1" goto prepare
+if "%num%"=="2" goto exit
+if "%num%"=="0" goto clean
+echo 错误的选择！
+pause
+goto start
+
+:prepare
+cls
+call "clean.bat"
+xelatex prepare
+bibtex prepare
+xelatex prepare
+xelatex prepare
+echo work down!
+pause
+goto start
+
+:clean
+call "clean.bat"
+echo work down!
+pause
+goto start
+
+:exit
+exit
