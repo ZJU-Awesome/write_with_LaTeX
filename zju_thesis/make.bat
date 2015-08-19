2b59ea2e4867f49e2acfd66f0e7ab08309cdba36
sky <diufanshu@gmail.com>
2015-08-19 22:56:47 +0800

    测试doc 为main.tex

--- zju_thesis/make.bat
+++ zju_thesis/make.bat
@@ -5,25 +5,25 @@ cls
 echo 欢迎使用 XeLaTeX 学位论文模板，请指示您的需要~
 echo ----------------
 echo 0. 清理冗余文件
-echo 1. 测试 prepare
+echo 1. 测试 main
 echo 2. 退出
 echo ----------------
 set /p num="请输入您的选择: "
-if "%num%"=="1" goto prepare
+if "%num%"=="1" goto main
 if "%num%"=="2" goto exit
 if "%num%"=="0" goto clean
 echo 错误的选择！
 pause
 goto start
 
-:prepare
+:main
 cls
 call "clean.bat"
-xelatex prepare
-bibtex prepare
-xelatex prepare
-xelatex prepare
-prepare.pdf
+xelatex main
+bibtex main
+xelatex main
+xelatex main
+main.pdf
 echo work down!
 pause
 goto start
